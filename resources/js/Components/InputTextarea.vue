69de1d57f986531098d6d8962000320b9ef3b722
giosurmanidze <giorgisurmanidze917@gmail.com>
2024-03-25 00:03:29 +0400

    style/group content

---
+++ resources/js/Components/InputTextarea.vue
@@ -0,0 +1,36 @@
+<script setup>
+import {onMounted, ref, watch} from 'vue';
+
+const props = defineProps({
+    modelValue: {
+        type: String,
+        required: false,
+    },
+    placeholder: String,
+    autoResize: {
+        type: Boolean,
+        default: true
+    }
+});
+
+const emit = defineEmits(['update:modelValue']);
+
+const input = ref(null);
+
+function onInputChange($event) {
+    emit('update:modelValue', $event.target.value)
+}
+
+
+
+</script>
+
+<template>
+    <textarea
+        class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm"
+        :value="modelValue"
+        @input="onInputChange"
+        ref="input"
+        :placeholder="placeholder"
+    ></textarea>
+</template>
--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,9 +1,23 @@
 <script setup>
 import { Head, Link } from "@inertiajs/vue3";
+import GroupList from "@/Components/app/GroupList.vue";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 
-defineProps({});
+defineProps({
+    groups: Array,
+});
 </script>
 
 <template>
     <Head title="Social Media Website" />
+
+    <!-- <AuthenticatedLayout> -->
+        <div class="grid lg:grid-cols-12 gap-3 p-4 h-full">
+            <div class="lg:col-span-3 lg:order-1 h-full overflow-hidden">
+                <GroupList :groups="groups" />
+            </div>
+        </div>
+    <!-- </AuthenticatedLayout> -->
 </template>
+
+<style></style>
