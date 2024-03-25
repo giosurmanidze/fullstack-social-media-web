15b115b8edd89307b6b8decd8b9a80185c715f4e
giosurmanidze <giorgisurmanidze917@gmail.com>
2024-03-25 16:37:58 +0400

    feat/profile page components

--- resources/js/Pages/Profile/Partials/UpdateProfileInformationForm.vue
+++ resources/js/Pages/Profile/Partials/UpdateProfileInformationForm.vue
@@ -17,8 +17,8 @@ defineProps({
 const user = usePage().props.auth.user;
 
 const form = useForm({
-    firstname: user.firstname,
-    lastname: user.lastname,
+    name: user.name,
+    username: user.username,
     email: user.email,
 });
 </script>
@@ -26,43 +26,41 @@ const form = useForm({
 <template>
     <section>
         <header>
-            <h2 class="text-lg font-medium text-gray-900">Profile Information</h2>
+            <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">Profile Information</h2>
 
-            <p class="mt-1 text-sm text-gray-600">
+            <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
                 Update your account's profile information and email address.
             </p>
         </header>
 
         <form @submit.prevent="form.patch(route('profile.update'))" class="mt-6 space-y-6">
             <div>
-                <InputLabel for="firstname" value="Firstname" />
+                <InputLabel for="name" value="Name" />
 
                 <TextInput
-                    id="firstname"
+                    id="name"
                     type="text"
                     class="mt-1 block w-full"
-                    v-model="form.firstname"
+                    v-model="form.name"
                     required
-                    autofocus
-                    autocomplete="firstname"
+                    autocomplete="name"
                 />
 
-                <InputError class="mt-2" :message="form.errors.firstname" />
+                <InputError class="mt-2" :message="form.errors.name" />
             </div>
             <div>
-                <InputLabel for="lastname" value="Lastname" />
+                <InputLabel for="username" value="Username" />
 
                 <TextInput
-                    id="lastname"
+                    id="username"
                     type="text"
                     class="mt-1 block w-full"
-                    v-model="form.lastname"
+                    v-model="form.username"
                     required
-                    autofocus
-                    autocomplete="lastname"
+                    autocomplete="username"
                 />
 
-                <InputError class="mt-2" :message="form.errors.lastname" />
+                <InputError class="mt-2" :message="form.errors.username" />
             </div>
 
             <div>
@@ -81,13 +79,13 @@ const form = useForm({
             </div>
 
             <div v-if="mustVerifyEmail && user.email_verified_at === null">
-                <p class="text-sm mt-2 text-gray-800">
+                <p class="text-sm mt-2 text-gray-800 dark:text-gray-200">
                     Your email address is unverified.
                     <Link
                         :href="route('verification.send')"
                         method="post"
                         as="button"
-                        class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                        class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
                     >
                         Click here to re-send the verification email.
                     </Link>
@@ -95,7 +93,7 @@ const form = useForm({
 
                 <div
                     v-show="status === 'verification-link-sent'"
-                    class="mt-2 font-medium text-sm text-green-600"
+                    class="mt-2 font-medium text-sm text-green-600 dark:text-green-400"
                 >
                     A new verification link has been sent to your email address.
                 </div>
@@ -110,7 +108,7 @@ const form = useForm({
                     leave-active-class="transition ease-in-out"
                     leave-to-class="opacity-0"
                 >
-                    <p v-if="form.recentlySuccessful" class="text-sm text-gray-600">Saved.</p>
+                    <p v-if="form.recentlySuccessful" class="text-sm text-gray-600 dark:text-gray-400">Saved.</p>
                 </Transition>
             </div>
         </form>
