2ea203f49e05cfe9b4cd0f0cbaff72c7f37f60e0
giosurmanidze <giorgisurmanidze917@gmail.com>
2024-03-25 16:39:01 +0400

    feat/error page

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,23 +1,37 @@
 <script setup>
-import { Head, Link } from "@inertiajs/vue3";
+import {Head, Link} from '@inertiajs/vue3';
 import GroupList from "@/Components/app/GroupList.vue";
+import FollowingList from "@/Components/app/FollowingList.vue";
+import CreatePost from "@/Components/app/CreatePost.vue";
+import PostList from "@/Components/app/PostList.vue";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 
 defineProps({
+    posts: Object,
     groups: Array,
+    followings: Array
 });
 </script>
 
 <template>
-    <Head title="Social Media Website" />
+    <Head title="Social Media Website"/>
 
-    <!-- <AuthenticatedLayout> -->
+    <AuthenticatedLayout>
         <div class="grid lg:grid-cols-12 gap-3 p-4 h-full">
             <div class="lg:col-span-3 lg:order-1 h-full overflow-hidden">
-                <GroupList :groups="groups" />
+                <GroupList :groups="groups"/>
+            </div>
+            <div class="lg:col-span-3 lg:order-3 h-full overflow-hidden">
+                <FollowingList :users="followings"/>
+            </div>
+            <div class="lg:col-span-6 lg:order-2 h-full overflow-hidden flex flex-col">
+                <CreatePost/>
+                <PostList :posts="posts.data" class="flex-1"/>
             </div>
         </div>
-    <!-- </AuthenticatedLayout> -->
+    </AuthenticatedLayout>
 </template>
 
-<style></style>
+<style>
+
+</style>
